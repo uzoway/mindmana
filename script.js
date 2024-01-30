da55abd4c22b9df972d7a7f3f7b3390282d0fdb3
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-01-30 23:09:56 +0100

    feat: Add the card shuffle functionality on click of the shuffle deck button

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
 })
+
+
+// Card Toggle Functionality
+
+const cardsParent = document.querySelector(".cards__container");
+const shuffleButton = document.querySelector("#shuffle__card");
+
+[...cardsParent.children].reverse().forEach(i => cardsParent.append(i));
+
+shuffleButton.addEventListener("click", shuffleCard);
+
+function shuffleCard(e) {
+    let currentCard = document.querySelector(".cards__container--item:last-child");
+
+    currentCard.style.animation = "shuffle 700ms forwards";
+
+    setTimeout(() => {
+        currentCard.style.animation = "";
+        cardsParent.prepend(currentCard);
+    }, 700);
+}
